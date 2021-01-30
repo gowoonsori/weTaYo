cd1431226562edd0b8845e0df9a486631b351748
gowoonsori <gowoonsori97@naver.com>
2021-01-30 18:08:45 +0900

    Add : deploy.sh

--- wetayo-api/scripts/run_new_was.sh
+++ wetayo-api/scripts/run_new_was.sh
@@ -19,6 +19,6 @@ if [ ! -z ${TARGET_PID} ];
     sudo kill ${TARGET_PID} 
 fi 
 
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/wetayo-api/build/libs/* > /home/ubuntu/nohup.out 2>&1 & 
+nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/wetayo-api/target/* > /home/ubuntu/nohup.out 2>&1 & 
 echo "> Now new WAS runs at ${TARGET_PORT}." 
 exit 0
