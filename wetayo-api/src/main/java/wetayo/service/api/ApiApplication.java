23a5e3662fe8197917853d1ce0c0b1998ccbcc94
gowoonsori <gowoonsori97@naver.com>
2021-01-30 18:37:10 +0900

    Add : properties

--- wetayo-api/src/main/java/wetayo/service/api/ApiApplication.java
+++ wetayo-api/src/main/java/wetayo/service/api/ApiApplication.java
@@ -4,8 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = { "classpath:jdbc.properties" })
 public class ApiApplication {
     @Bean
     public ModelMapper modelMapper(){
