79b5854ceb5b5b3f7b418e4fcfaba9a9c587052a
gowoonsori <gowoonsori97@naver.com>
2021-02-04 19:56:53 +0900

    init

--- wetayo-api/src/test/java/wetayo/service/api/ApiApplicationTests.java
+++ wetayo-api/src/test/java/wetayo/service/api/ApiApplicationTests.java
@@ -2,8 +2,15 @@ package wetayo.service.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+<<<<<<< HEAD
 
 @SpringBootTest
+=======
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootTest
+@PropertySource(value = { "classpath:jdbc.properties" })
+>>>>>>> 253f59861aaf93c15acfe99a6556046cf46a8fe4
 class ApiApplicationTests {
 
     @Test
